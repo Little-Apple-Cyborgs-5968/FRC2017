3502efeb0dd7c4dac2430d8ec2cbab116273532b
BeijingStrongbow <ericdhiggins@gmail.com>
2017-01-15 13:35:22 -0600

    Did some debugging. Still needs to be tested on the robot.

--- src/org/usfirst/frc/team5968/robot/PositionTracker.java
+++ src/org/usfirst/frc/team5968/robot/PositionTracker.java
@@ -15,7 +15,7 @@ public class PositionTracker {
 	
 	private static final double MOVEMENT_THRESHOLD = .05;
 	
-	private static final double STRAIGHT_LINE_THRESHOLD = .025;
+	private static final double STRAIGHT_LINE_THRESHOLD = .5;
 	
 	private static final double ROBOT_WIDTH = 14.031; //inches
 	
@@ -36,49 +36,46 @@ public class PositionTracker {
 	
 	
 	private static void updateCoordinates(){
-		double xDisplacementFromAccel = NavXMXP.getDisplacementX();
-		double yDisplacementFromAccel = NavXMXP.getDisplacementY();
-		double leftEncoderDistance = DriveBase.getLeftDistance();
-		double rightEncoderDistance = DriveBase.getRightDistance();
-		double angleUnprocessed = NavXMXP.getYaw() * Math.PI / 180; //angle from -pi / 2 to pi / 2
-		double angle = angleUnprocessed;
+		double xDisplacementFromAccel = NavXMXP.getDisplacementX() * 39.3701; //convert meters to inches
+		double yDisplacementFromAccel = NavXMXP.getDisplacementY() * 39.3701; //convert meters to inches
+		double leftEncoderDistance = DriveBase.getLeftDistance(); //inches
+		double rightEncoderDistance = DriveBase.getRightDistance(); //inches
+		double angle = NavXMXP.getYaw() * Math.PI / 180; //angle from -pi / 2 to pi / 2
 		double avgDistance;
 		
 		//if both the accelerometer and encoders registered movement
 		if((Math.abs(leftEncoderDistance) >= MOVEMENT_THRESHOLD || Math.abs(rightEncoderDistance) >= MOVEMENT_THRESHOLD) && 
-				(Math.abs(xDisplacementFromAccel) >= MOVEMENT_THRESHOLD || Math.abs(yDisplacementFromAccel) >= MOVEMENT_THRESHOLD)){
-			
+				(Math.abs(xDisplacementFromAccel) >= MOVEMENT_THRESHOLD || Math.abs(yDisplacementFromAccel) >= MOVEMENT_THRESHOLD) &&
+				((leftEncoderDistance >= 0 && rightEncoderDistance >= 0) ||
+				(leftEncoderDistance <= 0 && rightEncoderDistance <= 0))){
 			//approximate path as a straight line
-			if(Math.abs(leftEncoderDistance) - Math.abs(rightEncoderDistance) < STRAIGHT_LINE_THRESHOLD){
+			if(Math.abs(leftEncoderDistance - rightEncoderDistance) < STRAIGHT_LINE_THRESHOLD){
 				avgDistance = (leftEncoderDistance + rightEncoderDistance) / 2;
-				
 				x += avgDistance * Math.sin(angle);
 				y += avgDistance * Math.cos(angle);
 			}
 			
 			//approximate path as a circular arc
 			else{
-				if(Math.abs(leftEncoderDistance) - Math.abs(rightEncoderDistance) > MOVEMENT_THRESHOLD){
+				if(leftEncoderDistance == -1 * rightEncoderDistance){
 					return;
 				}
-				
 				double rightRadius = (rightEncoderDistance * ROBOT_WIDTH) / (leftEncoderDistance - rightEncoderDistance);
 				double leftRadius;
 				
 				//convert angle to 0 to 2 pi radians
-				if(angle >= 0 && angle <= 90){
+				if(angle >= 0 && angle <= Math.PI / 2){
 					angle = Math.PI / 2 - angle;
 				}
-				else if(angle > 90){
-					angle = (3 * Math.PI) / 2 + Math.PI - angle;
+				else if(angle > Math.PI / 2){
+					angle = Math.PI + angle;
 				}
-				else if(angle >= -90 && angle < 0){
+				else if(angle >= -1 * Math.PI / 2 && angle < 0){
 					angle = Math.PI / 2 - angle; // angle is negative
 				}
 				else{ //if angle > -180
 					angle = Math.PI + (-1 * angle - Math.PI / 2);
 				}
-				
 				//the angle to the center of the circle
 				double theta;
 				double previousTheta;
@@ -96,33 +93,30 @@ public class PositionTracker {
 					theta = angle + Math.PI / 2;
 					previousTheta = previousAngle + Math.PI / 2;
 				}
-				
 				//the radius of the robot's turning path
 				double radius = (leftRadius + rightRadius) / 2;
-				
 				double initialX = radius * Math.cos(previousTheta);
 				double initialY = radius * Math.sin(previousTheta);
 				double x = radius * Math.cos(theta);
 				double y = radius * Math.sin(theta);
-				
 				double dX = x - initialX;
 				double dY = y - initialY;
 				
-				x += dX;
-				y += dY;
+				PositionTracker.x += dX;
+				PositionTracker.y += dY;
 			}
 		}
 		
 		//if the encoders didn't read movement but the accelerometer does
-		else if((Math.abs(leftEncoderDistance) < MOVEMENT_THRESHOLD && Math.abs(rightEncoderDistance) < MOVEMENT_THRESHOLD) &&
-				(Math.abs(xDisplacementFromAccel) >= MOVEMENT_THRESHOLD || Math.abs(yDisplacementFromAccel) >= MOVEMENT_THRESHOLD)){
+		else if(Math.abs(xDisplacementFromAccel) >= MOVEMENT_THRESHOLD || Math.abs(yDisplacementFromAccel) >= MOVEMENT_THRESHOLD){
 			x += xDisplacementFromAccel;
 			y += yDisplacementFromAccel;
 		}
 		
 		//do nothing if the encoders read movement but the accelerometer didn't
 		
-		previousAngle = angleUnprocessed;
+		previousAngle = angle;
+		System.out.println("x: " + x + " y: " + y);
 		DriveBase.resetEncoders();
 		NavXMXP.resetAccelerometer();
 	}
