8b3540ba3d2f40aac19b4b0fed0a4688c602d958
allen458 <allenyz458@gmail.com>
2017-01-20 16:55:50 -0600

    Created Compressor Method

--- src/org/usfirst/frc/team5968/robot/Pneumatics.java
+++ src/org/usfirst/frc/team5968/robot/Pneumatics.java
@@ -1,1 +1,17 @@
 
+
+
+public class Pneumatics{
+  
+  public static void Compressor(){
+    
+    Compressor compressor = new Compressor(0);
+    compressor.setClosedLoopControl(true);
+    
+  }
+  
+  public static void DoubleSolenoid(){
+    
+    
+    
+ 
