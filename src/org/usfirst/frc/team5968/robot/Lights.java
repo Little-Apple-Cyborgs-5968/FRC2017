f381e6eb1a50febf8c27e499f65237b517324154
mattewers20 <mattewers20@gmail.com>
2017-02-04 14:12:10 -0600

    Changed lights to RGBW

--- src/org/usfirst/frc/team5968/robot/Lights.java
+++ src/org/usfirst/frc/team5968/robot/Lights.java
@@ -18,18 +18,17 @@ public class Lights extends IterativeRobot {
 	PWM r = new PWM(0);
 	PWM g = new PWM(1);
 	PWM b = new PWM(2);
-	Pneumatics p = new Pneumatics();
 	int timer = 0;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void upBrightness() {
-	    for(int x =0; x<=255; x++){
-	    	r.setRaw(x);
-	    	g.setRaw(x);
-	    	b.setRaw(x);
-	    }
+    public void robotInit() {
+    for(int x =0; x<=255; x++){
+    	r.setRaw(1);
+    	g.setRaw(1);
+    	b.setRaw(1);
+    }
     }
     
 	/**
@@ -41,36 +40,23 @@ public class Lights extends IterativeRobot {
 	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
-    public void off() {
-    	r.setRaw(0);
-    	g.setRaw(0);
-    	b.setRaw(0);
-    	
+    public void autonomousInit() {
+    	g.setRaw(26);
     }
 
     /**
      * This function is called periodically during autonomous
      */
-    public void green() {
-    	g.setRaw(26);
+    public void autonomousPeriodic() {
+    	
     }
 
     /**
      * This function is called periodically during operator control
      */
-    public void pneumatics() {
+    public void teleopPeriodic() {
        
-    	if(p.isUp()) {
-    		r.setRaw(146);
-    		g.setRaw(14);
-    		b.setRaw(14);
-    	}
-    	if(!p.isUp())  {
-    		b.setRaw(87);
-    	}
     	
-    }
-    public void climbing() {
     	timer++;
         if ((timer / 1000) % 2 == 0) {
         	r.setRaw(1);
