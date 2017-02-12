b1026c93173cc3e2ab7e5ee1a572ac337a0b1ffe
SilentLoudMouth <nolanblankenau@gmail.com>
2017-02-12 13:17:38 -0600

    Pneumatics are working

--- src/org/usfirst/frc/team5968/robot/Pneumatics.java
+++ src/org/usfirst/frc/team5968/robot/Pneumatics.java
@@ -9,46 +9,40 @@ import edu.wpi.first.wpilibj.Compressor;
 public class Pneumatics{
   
 	private static DoubleSolenoid piston1 = new DoubleSolenoid(PortMap.portOf(PortMap.PCM.BACK_PISTON_1), PortMap.portOf(PortMap.PCM.BACK_PISTON_2));    //Eric maybe put this in PortMap?
-	private static DoubleSolenoid piston2 = new DoubleSolenoid(PortMap.portOf(PortMap.PCM.FRONT_PISTON_1), PortMap.portOf(PortMap.PCM.FRONT_PISTON_2));    //Eric maybe put this in PortMap?
+    private static DoubleSolenoid piston2 = new DoubleSolenoid(PortMap.portOf(PortMap.PCM.FRONT_PISTON_1), PortMap.portOf(PortMap.PCM.FRONT_PISTON_2));
 	private static boolean isUp = false;
-	
-	private static Compressor compressor;
 	
 	public static void init(){
 
-		compressor = new Compressor(PortMap.portOf(PortMap.CAN.PCM));   //Same with this?
+		Compressor compressor = new Compressor(PortMap.portOf(PortMap.CAN.PCM));   //Same with this?
 		compressor.setClosedLoopControl(true);
+    
 	}
   
 	public static boolean getIsUp() {
 		return isUp;
 	}
-	
+	//It would be better code design to have just one method that toggles the solenoid. See the note in HumanInterface
 	public static void DoubleSolenoidTOGGLE(){
-    	if(piston1.get() == DoubleSolenoid.Value.kForward){
-    		piston1.set(DoubleSolenoid.Value.kReverse);
-    		piston2.set(DoubleSolenoid.Value.kReverse);
-    	}
-    	else{
-    		piston2.set(DoubleSolenoid.Value.kForward);
-    		piston1.set(DoubleSolenoid.Value.kForward);
-    	}
+    		
+		if(isUp){
+			
+			piston2.set(DoubleSolenoid.Value.kForward);
+			Timer.delay(.35);
+			piston1.set(DoubleSolenoid.Value.kForward);
+			isUp = false;
+			
+		}
+		else{
+			
+			piston1.set(DoubleSolenoid.Value.kReverse);
+			Timer.delay(.425);
+			piston2.set(DoubleSolenoid.Value.kReverse);
+			isUp = true;
+			
+		}
     
 	}
   
-	public static void setSolenoidDown(){
-		piston1.set(DoubleSolenoid.Value.kForward);
-		piston2.set(DoubleSolenoid.Value.kForward);
-		if(piston1.get() == DoubleSolenoid.Value.kForward){
-			System.out.println("lskdj");
-		}
-		else{
-			System.out.println("slkdslkd");
-		}
-	}
-	public static void stopCompressor(){
-		compressor.stop();
-	}
+	
 }
-    
- 
